424c690c49d1961bca2152b7b8aaee95be287909
Marcus <juandbeta@gmail.com>
2024-06-30 01:59:34 +0200

    W03 Assignment: Country Page completed

---
+++ scripts/place.js
@@ -0,0 +1,22 @@
+const tempItem = document.querySelector("#temp");
+const conditionItem = document.querySelector("#condition");
+const windItem = document.querySelector("#wind");
+const windChillItem = document.querySelector("#windChill");
+
+const temp = 26;
+const condition = "Mostly cloudy";
+const windSpeed = 6;
+
+function calculateWindChill(temp, windSpeed)
+{
+    if (windSpeed < 4.8 && temp > 10)
+    {
+        windChill = 13.12 + 0.6215 * temp * (0.3965 * temp - 11.37) * windSpeed ** 0.16;
+        return windChill;
+    } else
+    {
+        return "N/A";
+    }
+}
+
+windChillItem.textContent = $`<b>Wind Chill: </b>${calculateWindChill(temp, windSpeed)}`;
