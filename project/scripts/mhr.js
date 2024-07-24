a692dba4115ddcad28e08261eadb5ae29a3b0b16
Marcus <juandbeta@gmail.com>
2024-07-24 07:49:45 +0200

    monsters page started

--- project/scripts/mhr.js
+++ project/scripts/mhr.js
@@ -13,3 +13,41 @@ function displayMenu()
         (menu.classList.replace("visible", "notVisible"));
     }
 }
+
+/* Monsters slideshow */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length)
+    {
+        slideIndex = 1
+    }
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++)
+    {
+        slides[i].classList.replace("visible", "notVisible");
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].classList.replace("notVisible", "visible");
+    dots[slideIndex-1].classList.add("active");
+}
+
